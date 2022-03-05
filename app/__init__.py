7f7b5afeaae35a19ecedc021bcf0c1de3c44c988
ian-otieno <ian.otieno@student.moringaschool.com>
2022-03-05 12:48:12 +0300

    add dependecies and installations to init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from .models import User, db
+from flask_mail import Mail
+from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_heroku import Heroku
+
 
 # Initializing application
 app = Flask(__name__)
