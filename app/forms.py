b644ba8606defd0c1ec0947bdfc1b637e4c25804
ian-otieno <ian.otieno@student.moringaschool.com>
2022-03-05 18:26:35 +0300

    add class registration form

--- app/forms.py
+++ app/forms.py
@@ -6,3 +6,11 @@ class LoginForm(FlaskForm):
     email = StringField(label='Email Address', validators=[DataRequired(), Email(message=' Please Enter A Valid Email Address!')], render_kw={"placeholder": "Email Address"})
     password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8, max=250,  message='Password strength must be between %(min)d and %(max)d characters!')], render_kw={"placeholder": "Password"})
     submit = SubmitField(label=('Log In'))
+
+class RegisterForm(FlaskForm):
+    first_name = StringField(label='First Name', validators=[DataRequired(), Length(min=2, max=250,  message='Name length must be between %(min)d and %(max)d characters!')], render_kw={"placeholder": "First Name"})
+    last_name = StringField(label='Last Name', validators=[DataRequired(), Length(min=2, max=250,  message='Name length must be between %(min)d and %(max)d characters!')], render_kw={"placeholder": "Last Name"})
+    email = StringField(label='Email Address', validators=[DataRequired(), Email(message='Enter A Valid Email Address!')], render_kw={"placeholder": "Email Address"})
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8, max=250,  message='Password strength must be between %(min)d and %(max)d characters!')], render_kw={"placeholder": "Password"})
+    confirm_password = PasswordField(label='Confirm Password', validators=[DataRequired(), EqualTo('password', message='The Passwords Entered Do Not Match!')], render_kw={"placeholder": "Confirm Password"})
+    submit = SubmitField(label=('Sign Up'))
