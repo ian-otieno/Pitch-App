1d9c34def31e069c1715623252ad7d0b76f4dd2b
ian-otieno <ian.otieno@student.moringaschool.com>
2022-03-05 18:42:21 +0300

    initialize applications

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,31 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_heroku import Heroku
 
-
 # Initializing application
 app = Flask(__name__)
 
 from app import views
+
+mail = Mail()
+login = LoginManager()
+
+load_dotenv('.env')
+app.config.from_pyfile('config.py')
+
+heroku = Heroku(app)
+migrate = Migrate(app, db)
+
+db = SQLAlchemy(app)
+db.init_app(app)
+
+login.init_app(app)
+login.login_view = 'login'
+
+mail.init_app(app)
+bcrypt = Bcrypt(app)
+
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
